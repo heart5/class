52f12e4adb7a1a5bb6f47366d3b574a15cdd06b9
heart5 <baiyefeng@gmail.com>
2026-04-17 18:10:08 +0800

    朵朵专用

---
+++ for_flower.py
@@ -0,0 +1,19 @@
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: -all
+#     formats: ipynb,py:percent
+#     notebook_metadata_filter: jupytext,-kernelspec,-jupytext.text_representation.jupytext_version
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+# ---
+
+# %%
+print("Hello, I'm here now.")
+
+# %%
+print("Hello, I'm here now.")
+
+# %%
