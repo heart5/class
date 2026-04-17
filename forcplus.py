7167b727e8c2b26d6aeee9aa43a441d1579b3233
heart5 <baiyefeng@gmail.com>
2026-04-17 18:10:55 +0800

    尝试让jupyterlab支持c语言，测试成功。给石头用

---
+++ forcplus.py
@@ -0,0 +1,38 @@
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: -all
+#     formats: ipynb,py:percent
+#     notebook_metadata_filter: jupytext,-kernelspec,-jupytext.text_representation.jupytext_version
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+# ---
+
+# %% [markdown]
+# # c++ start
+
+# %%
+#include <iostream>
+
+# %%
+std::cout<<"Hello world"<<std::endl;
+
+# %%
+#include <bits/stdc++.h>
+
+# %%
+using namespace std;
+
+# %%
+int main(){
+    cout << "Hello world!!!" << endl;
+    system("pause");
+    return 0;
+    }
+
+# %%
+main()
+
+# %%
